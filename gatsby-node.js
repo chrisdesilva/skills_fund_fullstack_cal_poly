1f4e8caf2ecc881944a9928d56742743a525a951
Chris DeSilva <chris.a.desilva@gmail.com>
2019-10-22 09:58:36 -0500

    Update redirect url

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
 exports.createPages = ({ graphql, actions }) => {
     const { createPage, createRedirect } = actions
  
-    createRedirect({ fromPath: 'https://calpolyextendedbootcamp.skills.fund/*', toPath: 'https://calpolybootcamp.skills.fund/:splat', isPermanent: true, force: true })
+    createRedirect({ fromPath: 'https://calpolybootcamp.skills.fund/*', toPath: 'https://calpolyextendedbootcamp.skills.fund/:splat', isPermanent: true, force: true })
     createRedirect({ fromPath: '/go', toPath: '/?utm_source=school&utm_medium=printedskfbrochure&utm_campaign=studentbrochure'})
     createRedirect({ fromPath: '/accepted', toPath: '/'})
    
